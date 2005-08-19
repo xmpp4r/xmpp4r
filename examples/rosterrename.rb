ccbb9516b8f374d32235394b32396f5d6c7102ca
Stephan M <astro@gna.org>
2005-08-19 22:26:30 +0000

    An example to rename a roster element
    git-svn-id: svn://svn.gna.org/svn/xmpp4r/trunk/xmpp4r@29 fc81a325-65fc-0310-ab21-b05fd605316e

---
+++ examples/rosterrename.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+$:.unshift '../lib'
+
+require 'xmpp4r'
+require 'xmpp4r/rosterquery'
+
+# Command line argument checking
+
+if ARGV.size < 4
+  puts("Usage: ./rosterrename.rb <your jid> <password> <jid to rename> <new name> [<group1> ... <groupn>]")
+  exit
+end
+
+# Building up the connection
+
+#Jabber::DEBUG = true
+
+jid = Jabber::JID.new(ARGV[0])
+
+cl = Jabber::Client.new(jid, false)
+cl.connect
+cl.auth(ARGV[1]) or raise "Auth failed"
+
+# The iq stanza
+iq = Jabber::Iq::new('set')
+# The new roster instance and item element
+iq.add(Jabber::RosterQuery.new).add(Jabber::RosterItem.new(ARGV[2], ARGV[3])).groups = ARGV[4..ARGV.size]
+
+# Sending the stanza
+cl.send(iq)
+
+# Don't look at the results:
+cl.close
