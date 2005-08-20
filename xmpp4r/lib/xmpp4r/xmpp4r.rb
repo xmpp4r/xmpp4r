7d3ccd12cd1218717845f6738d45964a92896697
Lucas Nussbaum <lnu@gna.org>
2005-08-20 19:47:27 +0000

    test commit, sorry for the noise
    git-svn-id: svn://svn.gna.org/svn/xmpp4r/trunk@33 fc81a325-65fc-0310-ab21-b05fd605316e

--- xmpp4r/lib/xmpp4r/xmpp4r.rb
+++ xmpp4r/lib/xmpp4r/xmpp4r.rb
@@ -1,6 +1,7 @@
 #  XMPP4R - XMPP Library for Ruby
 #  Copyright (C) 2004 Lucas Nussbaum <lucas@lucas-nussbaum.net>
 #  Released under GPL v2 or later
+#
 
 require 'xmpp4r/message'
 require 'xmpp4r/iq'
