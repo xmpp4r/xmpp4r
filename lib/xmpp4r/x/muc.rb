72381e9092107c306dcdfc5b6fc7b3634ea4caff
Stephan M <astro@spaceboyz.net>
2006-01-06 01:42:53 +0000

    Jabber::Helpers: - MUCBrowser - MUCClient - SimpleMUCClient Jabber::XMucUserInvite Jabber::XMuc#password Jabber::XMucUser#status_code mucinfo and mucsimplebot examples Documentation, documentation, documentation
    git-svn-id: svn+ssh://svn.gna.org/svn/xmpp4r/trunk/xmpp4r@180 fc81a325-65fc-0310-ab21-b05fd605316e

--- lib/xmpp4r/x/muc.rb
+++ lib/xmpp4r/x/muc.rb
@@ -5,13 +5,14 @@
 require 'xmpp4r/x'
 require 'xmpp4r/jid'
 require 'xmpp4r/x/mucuseritem'
+require 'xmpp4r/x/mucuserinvite'
 
 module Jabber
   ##
   # Class for <x/> elements
   # with namespace http://jabber.org/protocol/muc
   #
-  # See JEP-0058 for details
+  # See JEP-0045 for details
   class XMuc < X
     ##
     # Initialize an <x/> element
@@ -19,6 +20,23 @@ module Jabber
     def initialize
       super
       add_namespace('http://jabber.org/protocol/muc')
+    end
+
+    ##
+    # Text content of the <tt><password/></tt> element
+    def password
+      first_element_text('password')
+    end
+
+    ##
+    # Set the password for joining a room
+    # (text content of the <tt><password/></tt> element)
+    def password=(s)
+      if s
+        replace_element_text('password', s)
+      else
+        delete_elements('password')
+      end
     end
   end
 
@@ -42,8 +60,24 @@ module Jabber
     def typed_add(element)
       if element.kind_of?(REXML::Element) && (element.name == 'item')
         super(XMucUserItem::new.import(element))
+      elsif element.kind_of?(REXML::Element) && (element.name == 'invite')
+        super(XMucUserInvite::new.import(element))
       else
         super(element)
+      end
+    end
+
+    ##
+    # Retrieve the three-digit code in
+    # <tt><x xmlns='http://jabber.org/protocol/muc#user'><status code='...'/></x></tt>
+    # result:: [Fixnum] or nil
+    def status_code
+      e = nil
+      each_element('status') { |xe| e = xe }
+      if e and e.attributes['code'].size == 3 and e.attributes['code'].to_i != 0
+        e.attributes['code'].to_i
+      else
+        nil
       end
     end
   end
