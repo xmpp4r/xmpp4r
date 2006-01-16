52b808ed57c531902f9f7dc62f113ac916d464a1
Stephan M <astro@spaceboyz.net>
2006-01-16 14:01:30 +0000

    Split up SOCKS5Bytestreams and IBB into several files (TODO: Simplify SOCKSSocket)
    git-svn-id: svn+ssh://svn.gna.org/svn/xmpp4r/trunk/xmpp4r@190 fc81a325-65fc-0310-ab21-b05fd605316e

---
+++ lib/xmpp4r/helpers/ibb/initiator.rb
@@ -0,0 +1,25 @@
+module Jabber
+  module Helpers
+    ##
+    # Implementation of IBB at the initiator side
+    class IBBInitiator < IBB
+      ##
+      # Open the stream to the peer,
+      # waits for successful result
+      #
+      # May throw ErrorException
+      def open
+        iq = Iq.new(:set, @peer_jid)
+        open = iq.add REXML::Element.new('open')
+        open.add_namespace IBB::NS_IBB
+        open.attributes['sid'] = @session_id
+        open.attributes['block-size'] = @block_size
+
+        @stream.send_with_id(iq) { |answer|
+          answer.type == :result
+        }
+      end
+    end
+  end
+end
+
