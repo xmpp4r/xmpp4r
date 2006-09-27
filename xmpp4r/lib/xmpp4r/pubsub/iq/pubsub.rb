5f07657ff57d04a18e23cc9df90865856b40d9c3
Olli <peterlustig@gna.org>
2006-09-27 19:12:35 +0000

    adding some minor changes and new pubsub elements
    git-svn-id: svn://svn.gna.org/svn/xmpp4r/trunk@285 fc81a325-65fc-0310-ab21-b05fd605316e

--- xmpp4r/lib/xmpp4r/pubsub/iq/pubsub.rb
+++ xmpp4r/lib/xmpp4r/pubsub/iq/pubsub.rb
@@ -13,5 +13,51 @@ module Jabber
         super(true)
       end
     end
+
+
+    ##
+    # Item 
+    # One PubSub Item
+    class Item < XMPPElement
+      name_xmlns 'item'
+      def initialize(id)
+        super(true)
+	attributes['id'] = id
+      end
+      def get_id
+        attributes['id']
+      end
+      def set_id=(myid)
+        attributes['id'] = myid
+      end
+    end
+
+    ##
+    # Items
+    # a collection of Items
+    class Items < XMPPElement
+      name_xmlns 'items'
+      def initialize
+        super(true)
+      end
+      def get_node
+        attributes['node']
+      end
+      def set_node=(mynodename)
+        attributes['node'] = mynodename
+      end
+      def get_subid
+        attributes['subid']
+      end
+      def set_subid=(mysubid)
+        attributes['subid'] = mysubid
+      end
+      def get_max_items
+        attributes['max_items']
+      end
+      def set_max_items=(mymaxitems)
+        attributes['max_items'] = mymaxitems
+      end
+    end
   end
 end
