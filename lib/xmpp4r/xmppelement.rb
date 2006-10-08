ec540232f71bc02a0fbccb389ec0db76398b407f
Stephan M <astro@spaceboyz.net>
2006-10-08 12:18:27 +0000

    Documentation for XMPPElement
    git-svn-id: svn+ssh://svn.gna.org/svn/xmpp4r/trunk/xmpp4r@290 fc81a325-65fc-0310-ab21-b05fd605316e

--- lib/xmpp4r/xmppelement.rb
+++ lib/xmpp4r/xmppelement.rb
@@ -6,14 +6,36 @@ require 'xmpp4r/rexmladdons'
 
 module Jabber
   ##
-  # root class of all Jabber XML elements
+  # This class represents an XML element and provides functionality
+  # for automatic casting of XML element classes according to their
+  # element name and namespace.
+  #
+  # Deriving classes must met these criteria:
+  # * The element name and namespace must be specified by calling
+  #   the name_xmlns class method
+  # * The class constructor must be callable with no mandatory parameter
   class XMPPElement < REXML::Element
     @@name_xmlns_classes = {}
 
+    ##
+    # Specify XML element name and xmlns for a deriving class,
+    # this pair and the class will be added to a global pool
+    #
+    # If the namespace is nil the class is a "wildcard class"
+    # matching elements with any xmlns if no other class with
+    # that namespace was defined
     def self.name_xmlns(name, xmlns=nil)
       @@name_xmlns_classes[[name, xmlns]] = self
     end
 
+    ##
+    # Find the name and namespace for a given class.
+    # This class must have registered these two values
+    # by calling name_xmlns at definition time.
+    #
+    # Raises an exception if none was found
+    # klass:: [Class]
+    # result:: [String, String] name and namespace
     def self.name_xmlns_for_class(klass)
       klass.ancestors.each do |klass1|
         @@name_xmlns_classes.each do |name_xmlns,k|
@@ -26,6 +48,11 @@ module Jabber
       raise "Class #{klass} has not set name and xmlns"
     end
 
+    ##
+    # Find a class for given name and namespace
+    # name:: [String]
+    # xmlns:: [String]
+    # result:: A descendant of XMPPElement or REXML::Element
     def self.class_for_name_xmlns(name, xmlns)
       if @@name_xmlns_classes.has_key? [name, xmlns]
         @@name_xmlns_classes[[name, xmlns]]
@@ -36,6 +63,12 @@ module Jabber
       end
     end
 
+    ##
+    # Import another REXML::Element descendant to:
+    # * Either an element class that registered with
+    #   name and xmlns before
+    # * Or if none was found to the class itself
+    #   (you may call this class method on a deriving class)
     def self.import(element)
       klass = class_for_name_xmlns(element.name, element.namespace)
       if klass != self and klass.ancestors.include?(self)
@@ -45,6 +78,11 @@ module Jabber
       end
     end
 
+    ##
+    # Initialize this element, which will then be initialized
+    # with the name registered with name_xmlns.
+    #
+    # force_xmlns:: [Boolean] Whether this element is always built with an xmlns attribute
     def initialize(force_xmlns=false)
       name, xmlns = self.class::name_xmlns_for_class(self.class)
       super(name)
@@ -53,6 +91,11 @@ module Jabber
       end
     end
 
+    ##
+    # Add a child element which will be imported according to the
+    # child's name and xmlns
+    # element:: [REXML::Element] Child
+    # result:: [REXML::Element or descendant of XMPPElement] New child
     def typed_add(element)
       if element.kind_of? REXML::Element
         element_ns = (element.namespace.to_s == '') ? namespace : element.namespace
