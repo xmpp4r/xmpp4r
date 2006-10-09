14c93a927406a5923674391d4a131cd1f0c90b69
Olli <peterlustig@gna.org>
2006-10-09 19:27:18 +0000

    adding some nodebrowsing stuff
    git-svn-id: svn+ssh://svn.gna.org/svn/xmpp4r/trunk/xmpp4r@295 fc81a325-65fc-0310-ab21-b05fd605316e

---
+++ lib/xmpp4r/pubsub/helper/nodebrowser.rb
@@ -0,0 +1,48 @@
+# =XMPP4R - XMPP Library for Ruby                                                                                     
+# License:: Ruby's license (see the LICENSE file) or GNU GPL, at your option.                                         
+# Website::http://home.gna.org/xmpp4r/ 
+
+require 'xmpp4r/discovery'
+
+module Jabber
+  module PubSub
+    class NodeBrowser
+      ##
+      # Initialize a new NodeBrowser
+      # new(stream,pubsubservice)
+      # strem:: [Jabber::Stream]
+      def initialize(stream)
+        @stream = stream
+      end
+      
+      ##
+      # Retrive the name of a PubSub Service
+      # Throws an ErrorException when receiving                                                                       
+      # <tt><iq type='error'/></tt>                                                                                   
+      # jid:: [JID] Target entity (set only domain!)                                                                  
+      # return:: [String] or [nil]
+      
+      def nodes(jid)
+        iq = Iq.new(:get,jid)
+	iq.from = @stream.jid 
+	iq.add(Discovery::IqQueryDiscoItems.new)
+	nodes = []
+	err = nil
+	@stream.send_with_id(iq) { |answer|
+	  if answer.type == :result                                                                                   
+	    answer.query.each_element('item') { |item|
+	      nodes.push(item.node)
+	    }                                                                                                         
+	    true                                                                                                      
+	  elsif answer.type == :error                                                                                 
+	    err = answer.error                                                                                        
+	    true                                                                                                      
+	  else                                                                                                        
+	    false                                                                                                     
+	  end 
+        }
+	return nodes
+      end
+    end
+  end
+end
