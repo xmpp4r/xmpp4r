d6e36cd78ad2c0f6c54ecadc2ff0b4ff56cdcc33
Stephan M <astro@gna.org>
2007-05-22 15:13:00 +0000

    SASL and rexmladdons fixes by Toby (bugs #8998, #8999)
    git-svn-id: svn://svn.gna.org/svn/xmpp4r/trunk@368 fc81a325-65fc-0310-ab21-b05fd605316e

--- xmpp4r/lib/xmpp4r/sasl.rb
+++ xmpp4r/lib/xmpp4r/sasl.rb
@@ -57,7 +57,7 @@ module Jabber
       def auth(password)
         auth_text = "#{@stream.jid.strip}\x00#{@stream.jid.node}\x00#{password}"
         error = nil
-        @stream.send(generate_auth('PLAIN', Base64::encode64(auth_text).strip)) { |reply|
+        @stream.send(generate_auth('PLAIN', Base64::encode64(auth_text).gsub(/\s/, ''))) { |reply|
           if reply.name != 'success'
             error = reply.first_element(nil).name
           end
