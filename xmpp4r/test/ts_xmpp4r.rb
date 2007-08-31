539879a3f533ddcffae13749b586464a5254d593
Lucas Nussbaum <lnu@gna.org>
2007-08-31 10:45:22 +0000

    ts_xmpp4r.rb improved
    the testsuite is now built dynamically, so we no longer risk forgetting about test cases. it's also possible to exclude a test from a run (see the code)
    
    
    git-svn-id: svn://svn.gna.org/svn/xmpp4r/trunk@398 fc81a325-65fc-0310-ab21-b05fd605316e

--- xmpp4r/test/ts_xmpp4r.rb
+++ xmpp4r/test/ts_xmpp4r.rb
@@ -5,39 +5,40 @@ $:.unshift File.join(File.dirname(__FILE__), '..', 'test')
 $:.unshift File.join(File.dirname(__FILE__), 'lib')
 $:.unshift File.join(File.dirname(__FILE__), 'test')
 
-# This is allowed here, to make sure it's enabled in all tests.
+# This is allowed here, to make sure it's enabled in all test.
 Thread::abort_on_exception = true
 
 require 'xmpp4r'
+require 'find'
 
-require 'bytestreams/tc_ibb'
-require 'bytestreams/tc_socks5bytestreams'
-require 'delay/tc_xdelay'
-require 'muc/tc_muc_simplemucclient'
-require 'muc/tc_muc_mucclient'
-require 'muc/tc_mucowner'
-require 'tc_error'
-require 'tc_stream'
-require 'tc_idgenerator'
-require 'tc_iqquery'
-require 'tc_streamError'
-require 'tc_presence'
-require 'vcard/tc_iqvcard'
-require 'vcard/tc_helper'
-require 'roster/tc_iqqueryroster'
-require 'roster/tc_xroster'
-require 'roster/tc_helper'
-require 'rpc/tc_helper'
-require 'pubsub/tc_helper'
-require 'version/tc_iqqueryversion'
-require 'version/tc_helper'
-require 'tc_streamSend'
-require 'tc_jid'
-require 'tc_iq'
-require 'tc_client'
-require 'tc_callbacks'
-require 'tc_xmppstanza'
-require 'tc_message'
-require 'tc_class_names'
-require 'tc_rexml'
-require 'tc_streamComponent'
+# List files' basenames, not full path!
+# EXCLUDED_FILES = [ 'tc_muc_simplemucclient.rb' ]
+EXCLUDED_FILES = []
+
+tc_files = []
+tc_subdirs = []
+Find.find(File.dirname(__FILE__)) do |f|
+  if File::directory?(f)
+    if f == '.'
+      # do nothing
+    elsif File::basename(f) != '.svn'
+      tc_subdirs << f
+      Find.prune
+    end
+  elsif File::basename(f) =~ /^tc.*\.rb$/
+    tc_files << f
+  end
+end 
+
+tc_subdirs.each do |dir|
+  Find.find(dir) do |f|
+    if File::file?(f) and File::basename(f) =~ /^tc.*\.rb$/
+      tc_files << f
+    end
+  end
+end
+
+tc_files.each do |f|
+  next if EXCLUDED_FILES.include?(File::basename(f))
+  require f
+end
