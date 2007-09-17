705e6aaea09d1655eb8f817d64b3ee224543c6eb
Lucas Nussbaum <lucas@lucas-nussbaum.net>
2007-09-17 08:59:28 +0000

    removed private fix for entities escaping as this was fixed in ruby 1.8.5.
    git-svn-id: svn+ssh://svn.gna.org/svn/xmpp4r/trunk/xmpp4r@412 fc81a325-65fc-0310-ab21-b05fd605316e

--- lib/xmpp4r/rexmladdons.rb
+++ lib/xmpp4r/rexmladdons.rb
@@ -118,56 +118,6 @@ module REXML
   end
 end
 
-############################################################################
- 
-module REXML
-  class Text
-    # Escapes all possible entities
-    def Text::normalize( input, doctype=nil, entity_filter=nil )
-      copy = input
-      # Doing it like this rather than in a loop improves the speed
-      if doctype
-        # Replace all ampersands that aren't part of an entity
-        copy = copy.gsub( EREFERENCE, '&amp;' )
-        doctype.entities.each_value do |entity|
-          copy = copy.gsub( entity.value, 
-            "&#{entity.name};" ) if entity.value and 
-              not( entity_filter and entity_filter.include?(entity) )
-        end
-      else
-        # Replace all ampersands
-        copy = copy.gsub( '&', '&amp;' )
-        DocType::DEFAULT_ENTITIES.each_value do |entity|
-          copy = copy.gsub(entity.value, "&#{entity.name};" )
-        end
-      end
-      copy
-    end
-  end
-
-	class Attribute
-		def initialize( first, second=nil, parent=nil )
-			@normalized = @unnormalized = @element = nil
-			if first.kind_of? Attribute
-				self.name = first.expanded_name
-				@value = first.value
-				if second.kind_of? Element
-					@element = second
-				else
-					@element = first.element
-				end
-			elsif first.kind_of? String
-				@element = parent if parent.kind_of? Element
-				self.name = first
-				@value = Text::normalize(second.to_s)
-			else
-				raise "illegal argument #{first.class.name} to Attribute constructor"
-			end
-		end
-  end
-
-end
-
 # Restore the old $VERBOSE setting
 $VERBOSE = oldverbose
 
