8cbc75d4d74c322c52caea0966c25b3d9b1b3142
Olli <peterlustig@gna.org>
2008-02-07 23:21:54 +0000

    some minor changes - need_approval? and need_configuration? added
    git-svn-id: svn+ssh://svn.gna.org/svn/xmpp4r/trunk/xmpp4r@454 fc81a325-65fc-0310-ab21-b05fd605316e

--- lib/xmpp4r/pubsub/stanzas/subscription.rb
+++ lib/xmpp4r/pubsub/stanzas/subscription.rb
@@ -17,27 +17,52 @@ module Jabber
 	subid =  mysubid
 	state = mysubscription
       end
+      
+      ##
+      # shows the jid
+      # return:: [String]
       def jid
         attributes['jid']
       end
+      
+      ##
+      # sets the jid
+      # =:: [Jabber::JID] or [String]
       def jid=(myjid)
         attributes['jid'] = myjid
       end
       
+      ##
+      # shows the node
+      # return:: [String]
       def node
         attributes['node']
       end
+      
+      ##
+      # sets the node
+      # =:: [String]
       def node=(mynode)
         attributes['node'] = mynode
       end
       
+      ##
+      # shows the subid
+      # return:: [String]
       def subid
         attributes['subid']
       end
+      
+      ##
+      # sets the subid
+      # =:: [String]
       def subid=(mysubid)
         attributes['subid'] = mysubid
       end
-       
+      
+      ##
+      # shows the state
+      # return:: [Symbol] (:none,:pending,:subscribed,:unconfigured) 
       def state                                                                                                            
           # each child of event
           # this should interate only one time
@@ -45,14 +70,33 @@ module Jabber
               when 'none'      		then return :none
               when 'pending'   		then return :pending
               when 'subscribed'         then return :subscribed
-              when 'unconfigured'       then return :items
+              when 'unconfigured'       then return :unconfigured
               else return nil
           end
       end
+      
+      ##
+      # sets the state
+      # =:: [String] or [Symbol]
       def state=(mystate)
-        attributes['subscription'] = mystate
+        attributes['subscription'] = mystate.to_s
       end
+      
       alias subscription state
+      
+      ##
+      # the subscription need a configuration?
+      # return:: true or false
+      def need_configuration?
+        self.first_element('subscribe-options').first_element('required') ? true : false
+      end
+      
+      ##
+      # is a approval from the nodeadmin needed?
+      # return:: true or false
+      def need_approval?
+        state == :pending ? true : false
+      end
     end
   end
 end
