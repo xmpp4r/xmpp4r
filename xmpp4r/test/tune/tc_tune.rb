fac349889bb66afa533efd1da3d36e819c0752b3
Mathew Wood <hellomatty@gna.org>
2008-02-14 13:16:34 +0000

    Initial support for XEP-0118 User Tune, w/ tests
    git-svn-id: svn://svn.gna.org/svn/xmpp4r/trunk@468 fc81a325-65fc-0310-ab21-b05fd605316e

---
+++ xmpp4r/test/tune/tc_tune.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/ruby
+
+$:.unshift '../lib'
+
+require 'test/unit'
+require 'xmpp4r/tune/tune.rb'
+
+class Jabber::UserTune::TuneTest < Test::Unit::TestCase
+  def test_create
+    artist='Mike Flowers Pops'
+    title='Light My Fire'
+    length=175
+    track='4'
+    source='A Groovy Place'
+    uri='http://musicbrainz.org/track/d44110e6-4b20-4d16-9e69-74bf0e4f7106.html'
+    rating=10
+
+    t=Jabber::UserTune::Tune.new(artist,title,length,track,source,uri,rating)
+
+    assert_kind_of Jabber::UserTune::Tune,t
+    assert_equal 7,t.elements.size
+    assert_equal true,t.playing?
+    assert_equal artist,t.artist
+    assert_equal track,t.track
+    assert_equal length,t.length
+    assert_equal track,t.track
+    assert_equal source,t.source
+    assert_equal uri,t.uri
+    assert_equal rating,t.rating
+  end
+
+  def test_stop_playing
+    t=Jabber::UserTune::Tune.new
+    
+    assert_kind_of Jabber::UserTune::Tune,t
+    assert_equal 0,t.elements.size
+    assert_equal false, t.playing?
+    assert_equal nil,t.artist
+    assert_equal nil,t.track
+    assert_equal nil,t.length
+    assert_equal nil,t.track
+    assert_equal nil,t.source
+    assert_equal nil,t.uri
+  end
+
+  def test_rating_edgecases
+    assert_equal(0, Jabber::UserTune::Tune.new(artist,title,length,track,source,uri,-1.5).rating)
+    assert_equal(10, Jabber::UserTune::Tune.new(artist,title,length,track,source,uri,11.5).rating)
+    assert_equal(nil, Jabber::UserTune::Tune.new(artist,title,length,track,source,uri,'fantastic').rating)
+  end
+  
+  def artist
+    'Mike Flowers Pops'
+  end
+
+  def title
+    'Light My Fire'
+  end
+
+  def length
+    175
+  end
+
+  def track
+    '4'
+  end
+
+  def source
+    'A Groovy Place'
+  end
+
+  def uri
+    'http://musicbrainz.org/track/d44110e6-4b20-4d16-9e69-74bf0e4f7106.html'
+  end
+
+  def rating
+    10
+  end
+end
