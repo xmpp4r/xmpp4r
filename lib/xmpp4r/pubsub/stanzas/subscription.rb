41d6b1fbfb2a7aa9cd449c55cbb9efebd94cc60b
Mathew Wood <hellomatty@gmail.com>
2008-02-26 23:12:58 +0000

    Support for XEP-0118, including: - a Helper for sending and receiving of User Tune events - supporting tests for above - basic examples for above - a couple of new pubsub stanza types, for example '<publish />'
    git-svn-id: svn+ssh://svn.gna.org/svn/xmpp4r/trunk/xmpp4r@470 fc81a325-65fc-0310-ab21-b05fd605316e

--- lib/xmpp4r/pubsub/stanzas/subscription.rb
+++ lib/xmpp4r/pubsub/stanzas/subscription.rb
@@ -9,60 +9,35 @@ module Jabber
     ##
     # Subscription
     class Subscription < XMPPElement
-      name_xmlns 'subscription' 
+      name_xmlns 'subscription', NS_PUBSUB
       def initialize(myjid=nil,mynode=nil,mysubid=nil,mysubscription=nil)
-        super()
-        jid = myjid if myjid
-	node =  mynode if mynode
-	subid =  mysubid  if mysubid
-	state = mysubscription if mysubscription
+        super(true)
+        jid = myjid
+	node =  mynode
+	subid =  mysubid
+	state = mysubscription
       end
-      
-      ##
-      # shows the jid
-      # return:: [String]
       def jid
-        JID::new(attributes['jid'])
+        attributes['jid']
       end
-      
-      ##
-      # sets the jid
-      # =:: [Jabber::JID] or [String]
       def jid=(myjid)
         attributes['jid'] = myjid
       end
       
-      ##
-      # shows the node
-      # return:: [String]
       def node
         attributes['node']
       end
-      
-      ##
-      # sets the node
-      # =:: [String]
       def node=(mynode)
         attributes['node'] = mynode
       end
       
-      ##
-      # shows the subid
-      # return:: [String]
       def subid
         attributes['subid']
       end
-      
-      ##
-      # sets the subid
-      # =:: [String]
       def subid=(mysubid)
         attributes['subid'] = mysubid
       end
-      
-      ##
-      # shows the state
-      # return:: [Symbol] (:none,:pending,:subscribed,:unconfigured) 
+       
       def state                                                                                                            
           # each child of event
           # this should interate only one time
@@ -70,26 +45,14 @@ module Jabber
               when 'none'      		then return :none
               when 'pending'   		then return :pending
               when 'subscribed'         then return :subscribed
-              when 'unconfigured'       then return :unconfigured
+              when 'unconfigured'       then return :items
               else return nil
           end
       end
-      
-      ##
-      # sets the state
-      # =:: [String] or [Symbol]
       def state=(mystate)
-        attributes['subscription'] = mystate.to_s
+        attributes['subscription'] = mystate
       end
-      
       alias subscription state
-      
-      ##
-      # is a approval from the nodeadmin needed?
-      # return:: true or false
-      def need_approval?
-        state == :pending ? true : false
-      end
     end
   end
 end
---
+++ test/tune/tc_helper_send.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/ruby
+
+$:.unshift File::dirname(__FILE__) + '/../../lib'
+
+require 'test/unit'
+require File::dirname(__FILE__) + '/../lib/clienttester'
+
+require 'xmpp4r'
+require 'xmpp4r/tune/helper/helper'
+require 'xmpp4r/tune/tune'
+include Jabber
+
+#Jabber::debug=true
+
+class UserTune::HelperTest < Test::Unit::TestCase
+  include ClientTester
+
+  ##
+  # Test sending 'now playing' notifications
+  #
+  # See http://www.xmpp.org/extensions/xep-0118.html#protocol-transport,
+  # example 1
+  def test_send_now_playing
+    artist = 'Mike Flowers Pops'
+    title = 'Light My Fire'
+    tune_to_send = UserTune::Tune.new(artist, title)
+
+    h = UserTune::Helper.new(@client, nil)
+    assert_kind_of(UserTune::Helper, h)
+
+    state { |now_playing|
+      assert_kind_of(Jabber::Iq, now_playing)
+      assert_equal :set, now_playing.type
+    
+      assert_kind_of(Jabber::PubSub::IqPubSub,now_playing.first_element('pubsub'))
+      assert_equal(Jabber::UserTune::NS_USERTUNE,now_playing.first_element('pubsub').first_element('publish').node)
+   
+      tune=now_playing.first_element('pubsub').first_element('publish').first_element('item').first_element('tune')
+      assert_kind_of Jabber::UserTune::Tune,tune
+      assert_equal true, tune.playing?
+      assert_equal artist,tune.artist
+      assert_equal title,tune.title
+      assert_equal nil,tune.length
+      assert_equal nil,tune.track
+      assert_equal nil,tune.source
+      assert_equal nil,tune.uri
+
+      send("<iq type='result' id='#{now_playing.id}'/>")
+    }
+    h.now_playing(tune_to_send)
+    wait_state
+  end
+
+  def test_send_stop_playing
+    h = UserTune::Helper.new(@client, nil)
+  
+    state { |now_playing|
+      tune = now_playing.first_element('pubsub').first_element('publish').first_element('item').first_element('tune')
+ 
+      assert_kind_of Jabber::UserTune::Tune, tune
+      assert_equal false, tune.playing?
+      assert_equal nil, tune.artist
+      assert_equal nil, tune.title
+      assert_equal nil,tune.length
+      assert_equal nil,tune.track
+      assert_equal nil,tune.source
+      assert_equal nil,tune.uri
+
+      send("<iq type='result' id='#{now_playing.id}'/>")
+    }
+    h.stop_playing
+    wait_state
+  end
+end
