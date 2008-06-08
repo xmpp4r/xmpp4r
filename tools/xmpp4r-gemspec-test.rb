3a57cf34c01b058d843bb2974658acb7dd1f4dcf
Glenn Rempe <glenn@rempe.us>
2008-06-08 13:45:07 -0700

    Moved GitHub gemspec test script to tools dir.
    Signed-off-by: Glenn Rempe <glenn@rempe.us>

--- tools/xmpp4r-gemspec-test.rb
+++ tools/xmpp4r-gemspec-test.rb
@@ -4,8 +4,8 @@
 # will simulate running in their environment.
 
 require 'rubygems/specification'
-data = File.read('xmpp4r.gemspec')
+data = File.read('../xmpp4r.gemspec')
 spec = nil
 Thread.new { spec = eval("$SAFE = 3\n#{data}") }.join
-puts spec.inspect
+puts spec
 
