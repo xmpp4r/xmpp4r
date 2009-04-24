376f198d6d5fd70aa134bb5e154cf6a00f60c523
Jacob Burkhart <jacob@brontes3d.com>
2009-04-24 16:40:44 -0400

    listener_mocker needs to store mock_clients and tracker_of_callers at class level to work across mocked listeners

--- lib/xmpp4r/test/listener_mocker.rb
+++ lib/xmpp4r/test/listener_mocker.rb
@@ -3,11 +3,11 @@ module Jabber
     class ListenerMocker
   
       def self.with_socket_mocked(callback_proc)
-        TCPSocket.class_eval{ @@with_socket_mocked_callback_proc = callback_proc }
+        TCPSocket.class_eval{ ListenerMocker.with_socket_mocked_callback_proc = callback_proc }
         TCPSocket.class_eval do
           alias_method :initialize_old, :initialize
           def initialize(*args)
-            initialize_old(*args) if @@with_socket_mocked_callback_proc.call(args)
+            initialize_old(*args) if ListenerMocker.with_socket_mocked_callback_proc.call(args)
           end
         end
         yield
@@ -16,12 +16,16 @@ module Jabber
           alias_method :initialize, :initialize_old
         end
       end
-  
+      
+      class << self
+        attr_accessor :mock_clients, :tracker_of_callers
+      end
+      
       def self.mocker_proc
         Proc.new do      
           attr_accessor :messagecbs, :connected
-          @@mock_clients = {}
-          @@tracker_of_callers = {}
+          ListenerMocker.mock_clients ||= {}
+          ListenerMocker.tracker_of_callers ||= {}
       
           def connect
             Jabber::debuglog("(in mock) connected #{@jid.bare}")
@@ -29,8 +33,8 @@ module Jabber
           end
       
           def close!
-            @@mock_clients[@jid.bare.to_s] = nil
-            @@tracker_of_callers[@jid.bare.to_s] = nil
+            ListenerMocker.mock_clients[@jid.bare.to_s] = nil
+            ListenerMocker.tracker_of_callers[@jid.bare.to_s] = nil
             self.connected = false
           end
       
@@ -41,22 +45,22 @@ module Jabber
           def auth_nonsasl(password, digest=true)
             Jabber::debuglog("(in mock) authed #{@jid.bare}") 
       
-            if(@@mock_clients[@jid.bare.to_s])
+            if(ListenerMocker.mock_clients[@jid.bare.to_s])
               #raise a stack trace about multiple clients
               raise "\n\n ---> READ ME: this is actualy 2 stack traces: <---- \n\n"+
               "There is already a client connected on that jid #{@jid.bare.to_s}. "+
               "The mock library cannot support multiple listeners connected as the same user! originally called in:\n"+
-              @@tracker_of_callers[@jid.bare.to_s].backtrace.join("\n")+"\n\n second trace: \n"
+              ListenerMocker.tracker_of_callers[@jid.bare.to_s].backtrace.join("\n")+"\n\n second trace: \n"
             else
               #store a stack trace so that next time we have multiple client, we can alert about it...
               begin
                 throw "just saving a stack trace"
               rescue => e
-                @@tracker_of_callers[@jid.bare.to_s] = e
+                ListenerMocker.tracker_of_callers[@jid.bare.to_s] = e
               end
             end
 
-            @@mock_clients[@jid.bare.to_s] = self
+            ListenerMocker.mock_clients[@jid.bare.to_s] = self
             true
           end
       
@@ -67,9 +71,9 @@ module Jabber
               # unless xml.to
               #   raise "no jid!"
               # end
-              if @@mock_clients[xml.to.bare.to_s]
+              if ListenerMocker.mock_clients[xml.to.bare.to_s]
                 Jabber::debuglog("(in mock) to #{xml.to.bare.to_s}")
-                @@mock_clients[xml.to.bare.to_s].messagecbs.process(xml)
+                ListenerMocker.mock_clients[xml.to.bare.to_s].messagecbs.process(xml)
               else
                 Jabber::debuglog("(in mock) no client listening as #{xml.to.bare.to_s}")
               end
