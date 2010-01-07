xmpp4r/xmpp4r

commit 8f482fc5a09dadc047c222b884996393753a8c05

Added set_xhtml_body and xhtml_body= methods

lib/xmpp4r/rexmladdons.rb
@@ -25,13 +25,14 @@ module REXML
 
     ##
     # Replaces or adds a child element of name <tt>e</tt> with text <tt>t</tt>.
-    def replace_element_text(e, t)
-      el = first_element(e)
+    def replace_element_text(e, t, namespace = nil)
+      el = first_element(e, namespace)
       if el.nil?
         el = REXML::Element.new(e)
+        el.add_namespace(namespace)
+        el.add_text(t)
         add_element(el)
-      end
-      if t
+      else
         el.text = t
       end
       self

lib/xmpp4r/message.rb
@@ -114,6 +114,37 @@ module Jabber
     end
 
     ##
+    # Sets the message's xhtml body
+    #
+    # b:: [String] xhtml body to set
+    def xhtml_body=(b)
+      if b.kind_of? String
+        b = REXML::Text.new(b, false, nil, true, nil, %r/.^/)
+      end
+      
+      html = first_element('html', 'http://jabber.org/protocol/xhtml-im')
+      
+      if html
+        html.replace_element_text('body', b, 'http://www.w3.org/1999/xhtml')
+      else
+        el = REXML::Element.new('html')
+        el.add_namespace('http://jabber.org/protocol/xhtml-im')
+        el.replace_element_text('body', b, 'http://www.w3.org/1999/xhtml')
+        add_element(el)
+      end
+    end
+
+    ##
+    # Sets the message's xhtml body
+    #
+    # b:: [String] xhtml body to set
+    # return:: [REXML::Element] self for chaining
+    def set_xhtml_body(b)
+      self.xhtml_body = b
+      self
+    end
+
+    ##
     # sets the message's subject
     #
     # s:: [String] subject to set

commit bd1f54160653c34d98a600ad37dec50907325371

Fixed a bug introduced in 8f482fc5a09dadc047c222b884996393753a8c05

lib/xmpp4r/rexmladdons.rb
@@ -30,9 +30,9 @@ module REXML
       if el.nil?
         el = REXML::Element.new(e)
         el.add_namespace(namespace)
-        el.add_text(t)
         add_element(el)
-      else
+      end
+      if t
         el.text = t
       end
       self